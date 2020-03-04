4941846d3f7720b350e52be6e8cf7648de8309b5
muhammad.sultan01 <muhammad.sultan96@gmail.com>
2020-03-04 12:32:32 -0600

    Reworking project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CustomerComponent } from './customer/customer.component';
-import { Customer } from './customer/customer';
 
-import { AppComponent } from './app.component';
-import { GuardserviceService } from './guardservice.service';
-
-
-const routes: Routes = [
-
-  
-  {path:'customer',component:CustomerComponent,canActivate:[GuardserviceService]},
-  {path:'customer/:id',component:CustomerComponent}
-  
-
-
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule 
-{
-
- }
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,54 +1,10 @@
 import { Component } from '@angular/core';
-import { Customer } from './customer/customer';
-import { Router } from '../../node_modules/@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  /*template:`
-     <p id="sampleId"> Welcome to Angular</p>
-  `,*/
- styleUrls: ['./app.component.css']
-/* styles:[`
-        p{
-          color:blue;
-          border: 2px solid red;
-        }
-        .sampleClass
-        {
-          color:red;
-        }
-        #sampleId
-        {
-          color:#34AEB6;
-        }
- 
- `]*/
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FirstAngAppn';
-  parentCustName:string = "John@123";
-  custObjInParent:Customer;
-  custId:string="1111";
-
-  constructor(private router:Router)
-  {
-
-  }
-
-  parentMethodToReciveData($event)
-  {
-    this.custObjInParent = $event;
-    console.log(this.custObjInParent.custId+" "+this.custObjInParent.custName);
-  }
-  navigateToCustomer()
-  {
-      this.router.navigate(["/customer"]);
-  }
-  fetchCustomerDetails()
-  {
-      console.log(this.custId);
-      this.router.navigate(["/customer",this.custId])
-  }
+  title = 'verizon-frontend';
 }
-
