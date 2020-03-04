e42a059b6f86b76244a4e40c66819a8509552d59
muhammad.sultan01 <muhammad.sultan96@gmail.com>
2020-03-04 12:32:32 -0600

    Reworking project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to FirstAngApp!');
+    expect(page.getTitleText()).toEqual('Welcome to verizon-frontend!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { SalutationPipe } from './customer/salutation.pipe'
-import { CustomerserviceService } from './customer/customerservice.service';
-import { HttpClientModule } from '../../node_modules/@angular/common/http';
-import { GuardserviceService } from './guardservice.service';
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomerComponent,
-    SalutationPipe
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [
-    CustomerserviceService,
-    GuardserviceService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
