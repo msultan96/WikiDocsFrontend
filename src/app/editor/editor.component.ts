b39b16a3906c2c291f7c8bd3a2f810113740932a
Daniel Neal <danielneal@verybluemoon.net>
2020-03-05 13:34:23 -0600

    Changed base page

--- src/app/editor/editor.component.ts
+++ src/app/editor/editor.component.ts
@@ -12,7 +12,6 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
 	@Input() public configuration!: CloudServicesConfig;
 	@Input() public channelId!: string;
 	@Output() public ready = new EventEmitter<CKEditor5.Editor>();
-	@ViewChild( 'sidebar', { static: true } as any ) private sidebarContainer?: ElementRef<HTMLDivElement>;
 	@ViewChild( 'presenceList', { static: true } as any ) private presenceListContainer?: ElementRef<HTMLDivElement>;
 
 	public Editor = ClassicEditorBuild;
@@ -28,9 +27,6 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
 			collaboration: {
 				channelId: this.channelId
 			},
-			sidebar: {
-				container: this.sidebar,
-			},
 			presenceList: {
 				container: this.presenceList,
 			}
@@ -39,36 +35,28 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
 
 	// Note that Angular refs can be used once the view is initialized so we need to create
 	// these containers and use in the above editor configuration to workaround this problem.
-	private sidebar = document.createElement( 'div' );
-	private presenceList = document.createElement( 'div' );
 
-	private boundRefreshDisplayMode = this.refreshDisplayMode.bind( this );
+	private presenceList = document.createElement( 'div' );
 	private boundCheckPendingActions = this.checkPendingActions.bind( this );
 
 	public ngAfterViewInit() {
-		if ( !this.sidebarContainer || !this.presenceListContainer ) {
-			throw new Error( 'Div containers for sidebar or presence list were not found' );
+		if (!this.presenceListContainer ) {
+			throw new Error( 'Div containers for presence list were not found' );
 		}
-
-		this.sidebarContainer.nativeElement.appendChild( this.sidebar );
 		this.presenceListContainer.nativeElement.appendChild( this.presenceList );
 	}
 
 	public ngOnDestroy() {
-		window.removeEventListener( 'resize', this.boundRefreshDisplayMode );
 		window.removeEventListener( 'beforeunload', this.boundCheckPendingActions );
 	}
 
-	private onReady( editor: CKEditor5.Editor ) {
+	public onReady( editor: CKEditor5.Editor ) {
 		this.editor = editor;
 		this.ready.emit( editor );
 
 		// Prevent closing the tab when any action is pending.
 		window.addEventListener( 'beforeunload', this.boundCheckPendingActions );
 
-		// Switch between inline and sidebar annotations according to the window size.
-		window.addEventListener( 'resize', this.boundRefreshDisplayMode );
-		this.refreshDisplayMode();
 	}
 
 	private checkPendingActions( domEvt ) {
@@ -78,89 +66,7 @@ export class EditorComponent implements AfterViewInit, OnDestroy {
 		}
 	}
 
-	private refreshDisplayMode() {
-		const annotations = this.editor.plugins.get( 'Annotations' );
-		const sidebarElement = this.sidebarContainer.nativeElement;
-
-		if ( window.innerWidth < 1070 ) {
-			sidebarElement.classList.remove( 'narrow' );
-			sidebarElement.classList.add( 'hidden' );
-			annotations.switchTo( 'inline' );
-		}
-		else if ( window.innerWidth < 1300 ) {
-			sidebarElement.classList.remove( 'hidden' );
-			sidebarElement.classList.add( 'narrow' );
-			annotations.switchTo( 'narrowSidebar' );
-		}
-		else {
-			sidebarElement.classList.remove( 'hidden', 'narrow' );
-			annotations.switchTo( 'wideSidebar' );
-		}
-	}
-
 	private getInitialData() {
-		return `
-	<h2>Bilingual Personality Disorder</h2>
-
-	<figure class="image image-style-side">
-		<img src="https://c.cksource.com/a/1/img/docs/sample-image-bilingual-personality-disorder.jpg">
-		<figcaption>
-			One language, one person.
-		</figcaption>
-	</figure>
-
-	<p>
-		This may be the first time you hear about this made-up disorder but it actually isn’t so far from the truth. Even the studies
-		that were conducted almost half a century show that <strong>the language you speak has more effects on you than you realise</strong>.
-	</p>
-	<p>
-		One of the very first experiments conducted on this topic dates back to 1964.
-		<a href="https://www.researchgate.net/publication/9440038_Language_and_TAT_content_in_bilinguals">In the experiment</a>
-		designed by linguist Ervin-Tripp who is an expert in psycholinguistic and sociolinguistic studies, adults who are bilingual
-		in English in French were showed series of pictures and were asked to create 3-minute stories. In the end participants emphasized
-		drastically different dynamics for stories in English and French.
-	</p>
-	<p>
-		Another ground-breaking experiment which included bilingual Japanese women married to American men in San Francisco were asked
-		to complete sentences. The goal of the experiment was to investigate whether or not human feelings and thoughts are expressed
-		differently in <strong>different language mindsets</strong>.
-	</p>
-	<p>Here is a sample from the the experiment:</p>
-
-	<table>
-		<thead>
-			<tr>
-				<th></th>
-				<th>English</th>
-				<th>Japanese</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td>Real friends should</td>
-				<td>Be very frank</td>
-				<td>Help each other</td>
-			</tr>
-			<tr>
-				<td>I will probably become</td>
-				<td>A teacher</td>
-				<td>A housewife</td>
-			</tr>
-			<tr>
-				<td>When there is a conflict with family</td>
-				<td>I do what I want</td>
-				<td>It's a time of great unhappiness</td>
-			</tr>
-		</tbody>
-	</table>
-
-	<p>
-		More recent <a href="https://books.google.pl/books?id=1LMhWGHGkRUC">studies</a> show, the language a person speaks affects
-		their cognition, behaviour, emotions and hence <strong>their personality</strong>. This shouldn’t come as a surprise
-		<a href="https://en.wikipedia.org/wiki/Lateralization_of_brain_function">since wealready know</a> that different regions
-		of the brain become more active depending on the person’s activity at hand. Since structure, information and especially
-		<strong>the culture</strong> of languages varies substantially and the language a person speaks is an essential element of daily life.
-	</p>
-`;
+		return ``;
 	}
 }
